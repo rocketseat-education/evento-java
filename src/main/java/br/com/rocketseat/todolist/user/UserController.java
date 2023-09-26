5b83b758d28d6bcfb7e4f9b03ab03b7c1fd475d0
Vini Fraga <vinifragam@gmail.com>
2023-09-26 15:40:08 -0300

    09-repository

--- src/main/java/br/com/rocketseat/todolist/user/UserController.java
+++ src/main/java/br/com/rocketseat/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.rocketseat.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,20 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-  /**
-   * String (texto)
-   * Integer (int) numeros inteiros
-   * Double (double) número 0.0000
-   * Float (float) Números 0.000
-   * char (A C )
-   * Date (data)
-   * void
-   */
-  /*
-   * Body
-   */
+  
+  @Autowired
+  private IUserRepository userRepository;
+
   @PostMapping("/")
-  public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.getUsername());
+  public UserModel create(@RequestBody UserModel userModel) {
+    var userCreated = this.userRepository.save(userModel);
+    return userCreated;
   }
 }
