910131810a6129b4dca4a98b5b025ce3a3506288
Vini Fraga <vinifragam@gmail.com>
2023-09-26 15:00:16 -0300

    04-funcionalidade-usuario

---
+++ src/main/java/br/com/rocketseat/todolist/user/UserController.java
@@ -0,0 +1,33 @@
+package br.com.rocketseat.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Modificador
+ * public
+ * private
+ * protected
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+  /**
+   * String (texto)
+   * Integer (int) numeros inteiros
+   * Double (double) número 0.0000
+   * Float (float) Números 0.000
+   * char (A C )
+   * Date (data)
+   * void
+   */
+  /*
+   * Body
+   */
+  @PostMapping("/")
+  public void create(@RequestBody UserModel userModel) {
+    System.out.println(userModel.name);
+  }
+}
