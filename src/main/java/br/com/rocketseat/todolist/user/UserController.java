da3327d370d4c3858dc650d112a55e25fd688b66
Vini Fraga <vinifragam@gmail.com>
2023-09-26 15:07:24 -0300

    05-getter-setter

--- src/main/java/br/com/rocketseat/todolist/user/UserController.java
+++ src/main/java/br/com/rocketseat/todolist/user/UserController.java
@@ -28,6 +28,6 @@ public class UserController {
    */
   @PostMapping("/")
   public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.name);
+    System.out.println(userModel.getUsername());
   }
 }
--- src/main/java/br/com/rocketseat/todolist/user/UserModel.java
+++ src/main/java/br/com/rocketseat/todolist/user/UserModel.java
@@ -4,4 +4,28 @@ public class UserModel {
   private String username;
   private String name;
   private String password;
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getPassword() {
+    return password;
+  }
 }
