91445528c21d89ff3ef05b9675c2663266c0c603
Vini Fraga <vinifragam@gmail.com>
2023-09-26 17:20:35 -0300

    17-password-decode

--- src/main/java/br/com/rocketseat/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/rocketseat/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,12 @@ package br.com.rocketseat.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.rocketseat.todolist.user.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,6 +16,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+  @Autowired
+  private IUserRepository userRepository;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -35,12 +41,21 @@ public class FilterTaskAuth extends OncePerRequestFilter {
         System.out.println(username);
         System.out.println(password);
         // Validar usuário
+        var user = this.userRepository.findByUsername(username);
+        if(user == null) {
+          response.sendError(401, "Usuário sem autorização");
+        } else {
+          // Validar senha
+          var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+          if (passwordVerify.verified) {
+            filterChain.doFilter(request, response);
+          } else {
+            response.sendError(401);
+          }
+          // Segue viagem
 
-        // Validar senha
+        }
 
-        // Segue viagem
-
-        filterChain.doFilter(request, response);
   }
 
   
