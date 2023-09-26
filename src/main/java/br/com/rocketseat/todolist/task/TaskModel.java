9224804ab5442f5379878cba2784da76a86659b6
Vini Fraga <vinifragam@gmail.com>
2023-09-26 16:14:12 -0300

    13-tbl-task

---
+++ src/main/java/br/com/rocketseat/todolist/task/TaskModel.java
@@ -0,0 +1,33 @@
+package br.com.rocketseat.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+  @Id
+  @GeneratedValue(generator = "UUID")
+  private UUID id;
+  private String description;
+  
+  @Column(length = 50)
+  private String title;
+  private LocalDateTime startAt;
+  private LocalDateTime endAt;
+  private String priority;
+  
+  private UUID idUser;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
+}
