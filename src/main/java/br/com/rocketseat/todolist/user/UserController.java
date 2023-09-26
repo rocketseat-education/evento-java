bac4c186579341a3e18915bc49e12df7d734a2da
Vini Fraga <vinifragam@gmail.com>
2023-09-26 15:59:01 -0300

    11-Response-Entity

--- src/main/java/br/com/rocketseat/todolist/user/UserController.java
+++ src/main/java/br/com/rocketseat/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.rocketseat.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,15 +22,17 @@ public class UserController {
   private IUserRepository userRepository;
 
   @PostMapping("/")
-  public UserModel create(@RequestBody UserModel userModel) {
+  public ResponseEntity create(@RequestBody UserModel userModel) {
     var user = this.userRepository.findByUsername(userModel.getUsername());
     
     if (user != null) {
       System.out.println("Usuário Já existe.");
-      return null;
+      // Mensagem de erro
+      // Status Code
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
     }
     
     var userCreated = this.userRepository.save(userModel);
-    return userCreated;
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
   }
 }
