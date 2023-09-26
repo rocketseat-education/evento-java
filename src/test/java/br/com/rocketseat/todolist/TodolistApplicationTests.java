e605b59d56157dc5c865e4eef691d357ce43969b
Vini Fraga <vinifragam@gmail.com>
2023-09-26 12:47:04 -0300

    01-criando-estrutura-projeto

---
+++ src/test/java/br/com/rocketseat/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rocketseat.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
