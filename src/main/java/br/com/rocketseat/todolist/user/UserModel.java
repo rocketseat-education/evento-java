48c9a1a2e9703d8d11abc52654368cd3e25a8713
Vini Fraga <vinifragam@gmail.com>
2023-09-26 15:08:15 -0300

    06-lombok

--- src/main/java/br/com/rocketseat/todolist/user/UserModel.java
+++ src/main/java/br/com/rocketseat/todolist/user/UserModel.java
@@ -1,31 +1,11 @@
 package br.com.rocketseat.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
+  
   private String username;
   private String name;
   private String password;
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public String getPassword() {
-    return password;
-  }
 }
