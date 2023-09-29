19445e5a042c3ba91b6c44c34fee4d0c2f3fe080
Elias Gabriel <eliasgabrielcf@gmail.com>
2023-09-29 10:48:50 -0300

    23-update-parcial

--- src/main/java/br/com/rocketseat/todolist/task/TaskController.java
+++ src/main/java/br/com/rocketseat/todolist/task/TaskController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.rocketseat.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -52,9 +53,10 @@ public class TaskController {
 
   @PutMapping("/{id}")
   public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
-    var idUser = request.getAttribute("idUser");
-    taskModel.setIdUser((UUID) idUser);
-    taskModel.setId(id);
-    return this.taskRepository.save(taskModel);
+    var task = this.taskRepository.findById(id).orElse(null);
+
+    Utils.copyNonNullProperties(taskModel, task);
+
+    return this.taskRepository.save(task);
   }
 }
